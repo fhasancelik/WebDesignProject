20cf4ce51567927f9e6b9a55bcc7c60c0db2abe3
fhasancelik <hasancelikjob@gmail.com>
2023-04-01 12:59:03 +0300

    main components created

---
+++ main.js
@@ -0,0 +1,9 @@
+let menuBtn=document.getElementById("navbutton")
+let menuBar=document.getElementById("navbar")
+
+menuBtn.addEventListener("click",()=>{
+menuBar.classList.toggle("openminimenu")
+
+})
+
+
